8189c65a525a16338181e2e746fa34e642a1a8e5
Alexis Munsayac <alexis.munsayac@gmail.com>
2022-10-21 18:43:04 +0800

    Bump

--- examples/demo/src/App.tsx
+++ examples/demo/src/App.tsx
@@ -6,8 +6,16 @@ export default function App(): JSX.Element {
   // eslint-disable-next-line no-unused-expressions
   autoAnimate;
 
-  const [items, setItems] = createSignal([0, 1, 2]);
-  const add = () => setItems((current) => [...current, current.length]);
+  let index = 0;
+
+  const [items, setItems] = createSignal<number[]>([]);
+  const add = () => {
+    setItems((current) => [...current, index]);
+    index += 1;
+  };
+  const remove = (selected: number) => {
+    setItems((current) => current.filter((item) => item !== selected));
+  };
 
   return (
     <div
@@ -20,7 +28,9 @@ export default function App(): JSX.Element {
               <li>
                 <button
                   type="button"
-                  onClick={() => alert(`You clicked Item Number ${item}`)}
+                  onClick={() => {
+                    remove(item);
+                  }}
                   class="px-4 py-2 text-sm font-medium text-white bg-white rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
                 >
                   Item Number {item}
